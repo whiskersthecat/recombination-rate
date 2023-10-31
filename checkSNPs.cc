535eb06cf065219f3d6ed9042d94a59a5fd2c44a
whiskersthecat <60399984+whiskersthecat@users.noreply.github.com>
2023-10-31 12:34:39 -0700

    Both Parents Analyzed
    checkSNPs.cc now takes two .bed files and two .SAM files, and using the consensus calling from both analyses

--- checkSNPs.cc
+++ checkSNPs.cc
@@ -11,56 +11,47 @@
 #include <sys/stat.h>
 using namespace std;
 
-/* Input: two SAM files and a .bed file
+// compile: g++ checkSNPs.cc -std=c++11 -o checkSNPs
+
+/* Input: two SAM files and two .bed files
 .bed file:
-chromosome mismatch_location1 mismatch_location2 base1 base2
+chromosome mismatch_location1 mismatch_location1+1 base1 base2
 
 First parse the .bed file on chromosome x to find a list:
 a) locations on chromosome x within alignment that should have a SNP
 Note: MD and alignment are all based on REFERENCE
+
+
 
 two SAM files aligned with minimap2
 1st SAM: UC96 reads aligned to Salinas
 2nd SAM: UC96 reads aligned to UC96
 
 READNAME CHR_ALIGNED START END MMLOC, MMLOC, ...
-
 */
 
-map<string, int> chromosome_names_1 = {
-{"Lsat_1_v11_chr9", 9},
-{"Lsat_1_v11_chr8", 8},
-{"Lsat_1_v11_chr7", 7},
-{"Lsat_1_v11_chr6", 6},
-{"Lsat_1_v11_chr5", 5},
-{"Lsat_1_v11_chr4", 4},
-{"Lsat_1_v11_chr3", 3},
-{"Lsat_1_v11_chr2", 2},
-{"Lsat_1_v11_chr1", 1},
-{"LSER_US96UC23_chr9", 9},
-{"LSER_US96UC23_chr8", 8},
-{"LSER_US96UC23_chr7", 7},
-{"LSER_US96UC23_chr6", 6},
-{"LSER_US96UC23_chr5", 5},
-{"LSER_US96UC23_chr4", 4},
-{"LSER_US96UC23_chr3", 3},
-{"LSER_US96UC23_chr2", 2},
-{"LSER_US96UC23_chr1", 1}
+map<string, int> chromosome_names = {
+{"SALH_C_Chr_9", 9},
+{"SALH_C_Chr_8", 8},
+{"SALH_C_Chr_7", 7},
+{"SALH_C_Chr_6", 6},
+{"SALH_C_Chr_5", 5},
+{"SALH_C_Chr_4", 4},
+{"SALH_C_Chr_3", 3},
+{"SALH_C_Chr_2", 2},
+{"SALH_C_Chr_1", 1},
+{"SERH_U_Chr_9", 9},
+{"SERH_U_Chr_8", 8},
+{"SERH_U_Chr_7", 7},
+{"SERH_U_Chr_6", 6},
+{"SERH_U_Chr_5", 5},
+{"SERH_U_Chr_4", 4},
+{"SERH_U_Chr_3", 3},
+{"SERH_U_Chr_2", 2},
+{"SERH_U_Chr_1", 1}
 };
 
-map<string, int> chromosome_names_2 = {
-{"LSER_US96UC23_chr9", 9},
-{"LSER_US96UC23_chr8", 8},
-{"LSER_US96UC23_chr7", 7},
-{"LSER_US96UC23_chr6", 6},
-{"LSER_US96UC23_chr5", 5},
-{"LSER_US96UC23_chr4", 4},
-{"LSER_US96UC23_chr3", 3},
-{"LSER_US96UC23_chr2", 2},
-{"LSER_US96UC23_chr1", 1}
-};
-
-static map<string, int>* chromosome_names_directory[] = {&chromosome_names_1, &chromosome_names_2};
+//static map<string, int>* chromosome_names_directory[] = {&chromosome_names_1, &chromosome_names_2};
 
 static int chromosome_sizes[] = { // (measured in millions of bases)
 258,
@@ -115,7 +106,10 @@ int main(int argc, char* argv[]) {
 	// Note: change to 2 input requirements
 
 	if(argc < 2) {
-		cout << "Usage: ./checkSNPS snp_file.bed samfile1.SAM samfile2.SAM" << endl;
+		//cout << "Usage: ./checkSNPS snp_file.bed samfile1.SAM samfile2.SAM" << endl;
+		cout << "Usage: ./checkSNPS snp_file1.bed snp_file2.bed samfile1.SAM samfile2.SAM" << endl;
+		cout << "    snp_file1: align US to Salinas" << endl;
+		cout << "    samfile1: align US to Salinas" << endl;
 		exit(1);
 	}
 
@@ -137,55 +131,73 @@ int main(int argc, char* argv[]) {
 
 	// at each location in this array, it states whether or not there is an expected SNP
 	static bool** mismatch_locations[] = {mismatch_locations_1, mismatch_locations_2};
-	ifstream snp_file(argv[1]);
-	if(!snp_file) {
-		cout << "SNP_file not good" << endl;
-		exit(1);
-	}
-	cout << "2. Reading in SNP_file" << endl;
-	while(snp_file) {
-		// read in the SNP_file
-		snp_file >> chr;
-		if(chr != prev_chr) {
-			cout << "Found SNP at position (last SNP):" << loc << " in chromosome " << current_chr_num << endl;
-			cout << "Reading SNPs for chromosome " << chr << endl;
-			current_chr_num ++;
-			if(current_chr_num > 9) {
-				cout << "Finished reading all 9 chromosomes" << endl;
-				break;
+	//ifstream snp_file(argv[1]);
+	
+	cout << "2. Reading in SNP_files..." << endl;
+	// Read in BOTH SNP (bed) files
+	for(int i = 0; i < 2; i++) {
+		current_chr_num = 0;
+		numPrints = 0;
+		ifstream snp_file(argv[1 + i]);
+		if(!snp_file) {
+			cout << "SNP_file " << i << " not good" << endl;
+			exit(1);
+		}
+		cout << " Reading SNP file number " << i + 1 << endl;
+
+		while(snp_file) {
+			// read in the SNP_file
+			snp_file >> chr;
+			if(chr != prev_chr) {
+				cout << "Found SNP at position (last SNP):" << loc << " in chromosome " << current_chr_num << endl;
+				cout << "Reading SNPs for chromosome " << chr << endl;
+				current_chr_num ++;
+				if(current_chr_num > 9) {
+					cout << "Finished reading all 9 chromosomes" << endl;
+					break;
+				}
+				numPrints = 0;
 			}
-			numPrints = 0;
-		}
 
-		// here, it matters which order the files are given
-		snp_file >> loc;
-		mismatch_locations_2[current_chr_num - 1][loc + 1] = 1;
-		if(numPrints % 100000 == 0) {
-			cout << "Found SNPs at positions: " << loc + 1;
-		}
+			// here, it matters which order the files are given
+			// snp_file >> loc;
+			// mismatch_locations_2[current_chr_num - 1][loc + 1] = 1;
+			// if(numPrints % 100000 == 0) {
+			// 	cout << "Found SNPs at positions: " << loc + 1;
+			// }
 
-		snp_file >> loc;
-		mismatch_locations_1[current_chr_num - 1][loc + 1] = 1;
-		if(numPrints % 100000 == 0) {
-			cout << " and " << loc + 1 << " in chromosome " << current_chr_num << endl;
-			
-		}
-		numPrints ++;
+			// snp_file >> loc;
+			// mismatch_locations_1[current_chr_num - 1][loc + 1] = 1;
+			// if(numPrints % 100000 == 0) {
+			// 	cout << " and " << loc + 1 << " in chromosome " << current_chr_num << endl;
+				
+			// }
 
-		snp_file >> c;
-		snp_file >> c;
-		prev_chr = chr;
+			snp_file >> loc;
+			// Should this be after reading the second location??
+			mismatch_locations[i][current_chr_num - 1][loc + 1] = 1;
+			if(numPrints % 100000 == 0) {
+			 	cout << "Found SNPs at positions: " << loc + 1 << " in chromosome " << current_chr_num << endl;
+			}
+			snp_file >> loc;
+
+			numPrints ++;
+
+			snp_file >> c;
+			snp_file >> c;
+			prev_chr = chr;
+		}
 	}
 
 	// PARSE, CHECK ALIGNED READS
-	ifstream sam_file_1(argv[2]);
-	ifstream sam_file_2(argv[3]);
+	ifstream sam_file_1(argv[3]);
+	ifstream sam_file_2(argv[4]);
 	if(!sam_file_1 || !sam_file_2) {
 		cout << "ERROR: one or both SAM file not good" << endl;
 		exit(1);
 	}
 
-	string dir_name = argv[2];
+	string dir_name = argv[3];
 	dir_name = dir_name + ".RRSUM";
 
 	mkdir((dir_name).c_str(), 0777);
@@ -228,36 +240,44 @@ int main(int argc, char* argv[]) {
 	ofstream AllDiscarded_reads(dir_name + "/AllDiscarded_Reads.txt");
 	ofstream AllBadMismatchNumber_reads(dir_name + "/AllBadMismatchNumber_reads.txt");
 
+	ofstream Expected4MM_numMM(dir_name + "/Expected4MM_numMM.txt");
+	ofstream Expected5MM_numMM(dir_name + "/Expected5MM_numMM.txt");
+	ofstream Expected6MM_numMM(dir_name + "/Expected6MM_numMM.txt");
+	ofstream Expected7MM_numMM(dir_name + "/Expected7MM_numMM.txt");
+	ofstream Expected8MM_numMM(dir_name + "/Expected8MM_numMM.txt");
 
+	ofstream* ExpectedxMM_numMM[] = {&Expected4MM_numMM,&Expected5MM_numMM,&Expected6MM_numMM,&Expected7MM_numMM,&Expected8MM_numMM};
 
 	if(!Recombinant_reads) {
 		cout << "cannot open ofstream" << endl;
 		exit(1);
 	}
 
-	cout << "Directory: " << dir_name << endl;
+	cout << "Output Directory: " << dir_name << endl;
 	//cout << "File: " << dir_name + "/Recombinant_Reads.txt" << endl;
 
 	//Recombinant_reads << "hi" << endl;
 	//exit(1);
 
 
-
+	// NOTE: both SAM files must have the reads in the exact same order; when being parsed here, it treats the next alignments in both
+	// 	sam files as being for the same read
 	int j = 0;
 	while(sam_file_1 && sam_file_2) {
 
-
-		for(int s = 0; s < 1; s++) {
+		for(int s = 0; s < 2; s++) { // changed to 2! Test both SAM files in a row
 			// 1. PARSE MD
 			mm_locations.clear();
 			*sam_files[s] >> name;
 			// Ignore secondary alignments (only take first)
-			while(name == prev_name && sam_file_1) {
+			while(name == prev_name && sam_files[s]) {
 				getline((*sam_files[s]), line);
 				*sam_files[s] >> name;
 			}
-			if(!sam_file_1)
+			if(!sam_files[s]) {
+				cout << "[DONE] Finished processing reads from at least one sam file" << endl;
 				break;
+			}
 			
 			OSS << endl << "####### Processing read " << name << " for parent s = " << s << ", parsing SAM..." << endl;
 			*sam_files[s]>> num; //  bitwise flag
@@ -347,7 +367,7 @@ int main(int argc, char* argv[]) {
 			double mismatch_rate;
 
 			//int chromosome_aligned = (*chromosome_names_directory[s])[chromosome];
-			int chromosome_aligned = chromosome_names_1[chromosome];
+			int chromosome_aligned = chromosome_names[chromosome];
 
 			int prev_result = -1;
 			int num_switches = 0;
@@ -357,6 +377,7 @@ int main(int argc, char* argv[]) {
 			OSS << "  >>Aligned to chromosome " << chromosome_aligned  << " (called " << chromosome << ")" << ", finding mismatches..." << endl;
 			if(chromosome_aligned > 0) {
 				for(int b = start_pos + 1; b < end_pos; b++) {
+					// iterate through the called SNPs to see if they actually occur
 					if(mismatch_locations[s][chromosome_aligned - 1][b] == 1) {
 						// should be a mismatch here
 						expected_mismatches ++;
@@ -382,23 +403,34 @@ int main(int argc, char* argv[]) {
 			} else {
 				OSS << "Chromosome did not map to chr(1-9), skipping analysis" << endl;
 			}
+
+			for(int i = 4; i < 9; i++) {
+				if(expected_mismatches == i) {
+					//*ExpectedxMM_numMM[i - 4] << name;
+					*ExpectedxMM_numMM[i - 4] << mm_locations.size() << endl;
+				}
+			}
 			
 			mismatch_rate = ((double)(mismatch) / (double)(match + mismatch));
 			OSS << endl << "Finished processing " << end_pos - start_pos << " length read from position " << start_pos << " to " << end_pos 
 				<< ": " << mismatch_rate * 100 << "% of expected mismatch locations are mismatches: ";
 			bool print_info = false;
-			if(expected_mismatches > mm_locations.size()) {
-				// filter out this read.
-				alignment_outcomes[s] = 5;
-				OSS << "Read is filtered out: more mismatches than expected:" << endl;
-				OSS << "   Num Expected:   " << expected_mismatches << endl;
-				OSS << "   Num Mismatches: " << mm_locations.size() << endl;
+			// POSSIBLE FILTER: remove reads with too many mismatches	
+			// if(expected_mismatches < mm_locations.size()) {
+			// 	// filter out this read.
+			// 	alignment_outcomes[s] = 5;
+			// 	OSS << "Read is filtered out: more mismatches than expected:" << endl;
+			// 	OSS << "   Num Expected:   " << expected_mismatches << endl;
+			// 	OSS << "   Num Mismatches: " << mm_locations.size() << endl;
 				
-			} else if(mismatch + match < 2){
+			// } else 
+			if(mismatch + match < 2){
+				print_info = false;
+				// too little spanned sites
 				alignment_outcomes[s] = 3;
 				// total_Discarded++;
 				//total_Recombinant++;
-			} else if(mismatch_rate <= 0.0001) {
+			} else if(mismatch_rate <= 0.0001) { // account for floating point precision
 				if(s == 0){ alignment_outcomes[0] = 0; OSS << "Read is Salinas" << endl;}
 				if(s == 1){ alignment_outcomes[1] = 1; OSS << "Read is US96UC23" << endl;}//countUS96UC23();
 				print_info = true;
@@ -433,11 +465,11 @@ int main(int argc, char* argv[]) {
 				OSS << endl;
 			}
 
-			if(s == 0) { // changed temporarily to s == 0
+			if(s == 1) { // changed temporarily to s == 0
 				prev_name = name;
 				if(alignment_outcomes[0] == 5 || alignment_outcomes[1] == 5) {
 					total_Bad_Mismatch_Number++;
-					OSS << " ******* Read is discarded, too many Mismatches" << endl << endl;
+					OSS << " ******* Read is discarded, too many Mismatches in one or more read" << endl << endl;
 					AllBadMismatchNumber_reads << OSS.str();
 				} else if(alignment_outcomes[0] == 4 || alignment_outcomes[1] == 4) {
 					total_Bad_Recombinant++;
@@ -447,17 +479,17 @@ int main(int argc, char* argv[]) {
 					total_Discarded++;
 					OSS << " ******* Read is discarded, one or more alignments failed to span more than one mismatch sites" << endl << endl;
 					AllDiscarded_reads << OSS.str();
-				} else if(alignment_outcomes[0] == 0 || alignment_outcomes[1] == 0) { // changed temporarily to or
+				} else if(alignment_outcomes[0] == 0 && alignment_outcomes[1] == 0) { // changed temporarily to or
 					total_Salinas++;
 					OSS << " ******* Read is SALINAS" << endl << endl;
 					Salinas_reads << OSS.str();
 				}
-				else if(alignment_outcomes[0] == 1 || alignment_outcomes[1] == 1) { // changed temporarily to or
+				else if(alignment_outcomes[0] == 1 && alignment_outcomes[1] == 1) { // changed temporarily to or
 					total_US96UC23++;
 					OSS << " ******* Read is US96UC23" << endl << endl;
 					US96UC23_reads << OSS.str();
 				}
-				else if(alignment_outcomes[0] == 2 || alignment_outcomes[1] == 2) { // changed temporarily to or
+				else if(alignment_outcomes[0] == 2 && alignment_outcomes[1] == 2) { // changed temporarily to or
 					total_Recombinant++;
 					OSS << " ******* Read is  >>>RECOMBINANT " << endl << endl;
 					Recombinant_reads << OSS.str();
@@ -471,10 +503,11 @@ int main(int argc, char* argv[]) {
 					AllDiscarded_reads << OSS.str();
 				}
 			}
-			//cout << OSS.str();
-			OSS.clear();
-			OSS.str("");
+			
 		}
+		cout << OSS.str();
+		OSS.clear();
+		OSS.str("");
 		
 	}
 	int total_reads = total_Salinas + total_US96UC23 + total_Recombinant + total_Bad_Recombinant + total_Discarded + total_Contradict + total_Bad_Mismatch_Number;
@@ -491,7 +524,6 @@ int main(int argc, char* argv[]) {
 	cout << "Total Bad Recombinant (at least one alignment is 'Recombinant' with more than 1 switch)  : " << total_Bad_Recombinant << endl;
 	cout << "Total Bad Alignment (at least one alignment <2 mismatch sites spanned or aligned to Chr0): " << total_Discarded << endl;
 	cout << "Total Contradictions (alignment conclusions are not in agreement)                        : " << total_Contradict << endl << endl;
-
 	
 	cout << "Processed [" << total_reads << "] reads, " <<  attrition_rate * 100 << " % were thrown out" << endl;
 	auto end = chrono::high_resolution_clock::now();
